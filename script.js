9db485d4fc5f3e4f55f31ae639378785999bd39d
ryotasaku <natadekoko123@gmail.com>
2025-09-10 16:25:20 +0900

    Add new logo images (23.png, 24.jpg, 25.jpg) and update image list

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const imageFiles = [
     '1.jpg', '2.png', '3.jpg', '4.png', '5.png', '6.jpg',
     '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg',
     '13.jpg', '14.png', '15.jpg', '16.jpg', '17.jpg', '18.png',
-    '19.jpeg', '20.jpg', '21.jpeg', '22.jpg'
+    '19.jpeg', '20.jpg', '21.jpeg', '22.jpg', '23.png', '24.jpg', '25.jpg'
 ];
 
 // 選択された画像を管理する配列
